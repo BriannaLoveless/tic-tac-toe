2c2d5e03dec95a972eea1ea1decea07041c93ed9
Brianna Loveless <briannabel97@gmail.com>
2022-04-21 15:48:30 -0500

    refactor

--- tictactoe.js
+++ tictactoe.js
@@ -3,10 +3,6 @@ const readline = require('readline').createInterface({
     output: process.stdout,
 });
 
-let count = 0;
-let player = 'X';
-let ticTacToe = [];
-let exampleBoard = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
 const winLines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -16,57 +12,26 @@ const winLines = [
     [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6],
-  ];
+];
 
-function intro() {
-    console.log("Let's play tic tac toe!");
-    console.log('How to: X goes first.');
-    console.log(
-        "Enter a number between 1 and 9 to choose where you you'd like to place your move."
-    );
-}
-
-function askReplay() {
-    readline.question(
-        'Play again? (y/n) ',
-        (answer) => {
-            if (answer === 'y'){
-                initializeGame()
-            } else if (answer === 'n') {
-                process.exit(0)
-            } else {
-                console.log("Sorry I didn't get that.")
-                askReplay()
-            }
-        })
-}
+let count = 0;
+let player = 'X';
+let ticTacToe = [];
+let exampleBoard = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 function initializeGame() {
-    count = 0
-    ticTacToe = []
-    player= 'X'
+    // set default values
+    count = 0;
+    ticTacToe = [];
+    player = 'X';
+    // draw example board
     drawBoard(exampleBoard);
+    // display intro
     intro();
-    askMove()
+    askMove();
 }
 
-function counter() {
-    count += 1;
-}
-
-function calculateWinner() {
-    for (let [a, b, c] of winLines) {
-        if (
-            ticTacToe[a] &&
-            ticTacToe[a] === ticTacToe[b] &&
-            ticTacToe[a] === ticTacToe[c]
-        ) {
-            return ticTacToe[a]
-        }
-    }
-    return ' '
-}
-
+// prints representation of the game state to the console
 function drawBoard(arr) {
     console.log();
     console.log('-------------');
@@ -81,50 +46,84 @@ function drawBoard(arr) {
     }
 }
 
+function intro() {
+    console.log("Let's play tic tac toe!");
+    console.log('How to: X goes first.');
+    console.log("Enter a number between 1 and 9 to choose where you you'd like to place your move.");
+}
+
+function askMove() {
+    // check tie
+    if (count >= 9) {
+        console.log('Its a draw!');
+        return askReplay();
+    }
+
+    // ask player for input
+    readline.question(`Where would you like to place ${player}? `, (slot) => {
+        // validate input
+        if (slot < 1 || slot > 9) {
+            console.log('please enter a number between 1 and 9');
+            askMove();
+        } else if (slot.length !== 1 || isNaN(parseInt(slot))) {
+            console.log("Sorry I didn't get that");
+            askMove();
+        } else {
+            play(slot);
+        }
+    });
+}
+
+function askReplay() {
+    readline.question('Play again? (y/n) ', (answer) => {
+        if (answer === 'y') {
+            initializeGame();
+        } else if (answer === 'n') {
+            process.exit(0);
+        } else {
+            console.log("Sorry I didn't get that.");
+            askReplay();
+        }
+    });
+}
+
 function play(slot) {
     // slot is the number picked
+    // validate slot is empty
     if (!ticTacToe[slot - 1]) {
         ticTacToe[slot - 1] = player;
-        counter();
-        if (player === 'X') {
-            player = 'O';
-        } else if (player === 'O') {
-            player = 'X';
-        }
+        // increment move count
+        count++;
+        // toggle player
+        player = player === 'X' ? 'O' : 'X';
     } else {
         console.log('please choose an unused slot');
     }
 
     drawBoard(ticTacToe);
+
+    // ask for replay if winner
     const winner = calculateWinner();
-    if (winner != ' ') {
+    if (winner) {
         console.log(`Winner is ${winner}!`);
-        askReplay();
+        return askReplay();
     }
-    
+
     askMove();
 }
 
-function askMove() {
-    if (count < 9) {
-        readline.question(
-            `Where would you like to place ${player}? `,
-            (slot) => {
-                if (slot < 1 || slot > 9) {
-                    console.log('please enter a number between 1 and 9');
-                    askMove();
-                } else if (slot.length !== 1 || isNaN(parseInt(slot))) {
-                    console.log("Sorry I didn't get that");
-                    askMove();
-                } else {
-                    play(slot);
-                }
-            }
-        );
-    } else {
-        console.log('Its a draw!');
-        askReplay();
+// checks if any win conditions have been met and returns winning player
+function calculateWinner() {
+    // loop through all win conditions
+    for (let [a, b, c] of winLines) {
+        // check if a, b, c are equal and not empty
+        if ( ticTacToe[a] && ticTacToe[a] === ticTacToe[b] && ticTacToe[a] === ticTacToe[c] ) {
+            // return winner
+            return ticTacToe[a];
+        }
     }
+    // return no winner
+    return null;
 }
 
-initializeGame()
+initializeGame();
